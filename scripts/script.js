f7f7e3c1eeca7c9c4e7c5578202367361a46c906
Chris <chrisdnielson@gmail.com>
2022-09-25 13:19:19 -0700

    Build getPlayerChoice function
    built a getPlayerChoice function, which prompts a user to input
    either 'rock', 'paper', or 'scissors'. The function tests and
    sanitizes the input to check if it's valid before returning a
    string value of 'rock', 'paper', or 'scissors'.

--- scripts/script.js
+++ scripts/script.js
@@ -3,4 +3,50 @@ function getComputerChoice() {
     return choice[Math.floor(Math.random() * 3)] //returns the 0th, 1st or 2nd index
 }
 
-console.log(getComputerChoice());
+function getPlayerChoice() {
+    while(true) {
+        const input = prompt("Rock, Paper or Scissors?");
+        const choice = input.toLowerCase();
+        if(choice === 'rock' || choice === 'paper' || choice === 'scissors') {
+            //valid input, return
+            return choice;
+        }
+        else {
+            alert("invalid choice, type 'rock', 'paper', or 'scissors'");
+        }
+    }
+}
+
+function playRound(computerChoice, playerChoice) {
+    //compare the player inputs to the computer's input
+    if(computerChoice === playerChoice) {
+        //tie game
+    }
+
+    if(computerChoice == 'rock') {
+        if(playerChoice == 'paper') {
+            
+        }
+        if(playerChoice == 'scissors') {
+            
+        }
+    }
+
+    if(computerChoice == 'paper') {
+        if(playerChoice == 'rock') {
+
+        }
+        if(playerChoice == 'scissors') {
+            
+        }
+    }
+
+    if(computerChoice == 'scissors') {
+        if(playerChoice == 'rock') {
+
+        }
+        if(playerChoice == 'paper') {
+            
+        }
+    }
+}
