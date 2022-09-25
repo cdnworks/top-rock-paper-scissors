588bdcbd3780baea22d6e3f4a9a26efd9b2ff27c
Chris <chrisdnielson@gmail.com>
2022-09-25 12:59:04 -0700

    Build getComputerChoice function
    built a function that randomly generates a 'choice' for the computer
    controlled player; selects from 'rock', 'paper', or 'scissors'

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,6 @@
-alert("NICE MODEL")
+function getComputerChoice() {
+    const choice = ['rock', 'paper', 'scissors'];
+    return choice[Math.floor(Math.random() * 3)] //returns the 0th, 1st or 2nd index
+}
+
+console.log(getComputerChoice());
